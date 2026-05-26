d292e0e517ccab040608061698f19b7d2197d314
joebarbaro21 <ps7nww5grg@privaterelay.appleid.com>
2026-05-26 16:01:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/Joe-Tracker-/",
+  base: "/Tracker-Fit-Joe",
 
   plugins: [
     react(),
